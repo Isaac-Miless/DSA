3713c5b16d289ba6d6fb1be4357f1ef3ce445f0a
Isaac <isaacmiles36@gmail.com>
2025-02-03 13:22:36 +0000

    Add reorderLinkedList (not solved)

---
+++ Linked List/reorderLinkedList.py
@@ -0,0 +1,10 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head) -> None:
+        # [0, n-1, 1, n-2, 2, n-3, ...]
+        pass
