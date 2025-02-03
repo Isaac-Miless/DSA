a8b1d13d0427672087a5ed494c40ba9b46b844da
Isaac <isaacmiles36@gmail.com>
2025-02-03 14:28:44 +0000

    Start reorderList

--- Linked List/reorderLinkedList.py
+++ Linked List/reorderLinkedList.py
@@ -7,4 +7,13 @@
 class Solution:
     def reorderList(self, head) -> None:
         # [0, n-1, 1, n-2, 2, n-3, ...]
-        pass
+        
+        # find the middle of the linked list
+        # to partition them
+        mid = end = head
+        while end and end.next:
+            mid = mid.next
+            end = end.next.next
+
+        print(head, mid, end)
+
