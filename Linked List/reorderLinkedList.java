b629526eb652bb58ae762dc7d9b8f0f0e3d9d37c
Isaac <ibm4@kent.ac.uk>
2025-02-04 22:58:03 +0000

    Add subclasses so that there aren't errors

--- Linked List/reorderLinkedList.java
+++ Linked List/reorderLinkedList.java
@@ -44,4 +44,21 @@ class Solution {
             secondHalf = temp2;
         }
     }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
