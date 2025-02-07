6fd77090d03fb5fb791bb9f7d9108e8103ecc36f
Isaac <ibm4@kent.ac.uk>
2025-02-07 11:07:24 +0000

    Add working solution

--- Arrays and Hashing/numberOfDistinctColoursAmongBalls.py
+++ Arrays and Hashing/numberOfDistinctColoursAmongBalls.py
@@ -1,4 +1,4 @@
-from collections import Counter, OrderedDict
+from collections import Counter
 class Solution(object):
     def queryResults(self, limit, queries):
         """
@@ -6,28 +6,48 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
-        dic = OrderedDict()
         res = []
-        color_count = Counter()
+        ball_map = {}  
+        color_freq = Counter()  
 
-        for ball, colour in queries:
-            if ball in dic:
-                old_colour = dic[ball]
-                color_count[old_colour] -= 1
-                if color_count[old_colour] == 0:
-                    del color_count[old_colour]
-            dic[ball] = colour
-            color_count[colour] += 1
+        for ball, color in queries:
+            if ball in ball_map:
+                old_color = ball_map[ball]
+                color_freq[old_color] -= 1
+                if color_freq[old_color] == 0:
+                    del color_freq[old_color]  
 
-            if len(dic) > limit:
-                _, firstCol = dic.popitem(last=False)
-                color_count[firstCol] -= 1
-                if color_count[firstCol] == 0:
-                    del color_count[firstCol]
-                    
-            res.append(len(color_count))
-                
+            ball_map[ball] = color
+            color_freq[color] += 1
+
+            res.append(len(color_freq))
+
         return res
+    
+    # def queryResults(self, limit, queries):
+    #     """
+    #     :type limit: int
+    #     :type queries: List[List[int]]
+    #     :rtype: List[int]
+    #     """
+    #     dic = OrderedDict()
+    #     res = []
+    #     distinct = set()
+
+    #     for ball, colour in queries:
+    #         if ball in dic:
+    #             if dic[ball] in distinct:
+    #                 distinct.remove(dic[ball])
+    #         dic[ball] = colour
+    #         distinct.add(colour)
+
+    #         if len(dic) > limit:
+    #             _, firstCol = dic.popitem(last=False)
+    #             if firstCol not in dic.values():
+    #                 distinct.remove(firstCol)
+    #         res.append(len(distinct))
+                
+    #     return res
 
 limit = 4
 queries = [[1,4],[2,5],[1,3],[3,4]]
@@ -38,3 +58,4 @@ print(Solution().queryResults(limit, queries)) # [1, 2, 2, 3, 4]
 
 queries = [[0,1],[1,4],[1,1],[1,4],[1,1]]
 print(Solution().queryResults(limit, queries)) # [1, 2, 1, 2, 1]
+
