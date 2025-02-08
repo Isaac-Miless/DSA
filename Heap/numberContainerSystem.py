5695d8144c1647aa8583ba482a377e39bae8ec64
Isaac <ibm4@kent.ac.uk>
2025-02-08 21:21:20 +0000

    Add daily LC (will solve tomorrow)

---
+++ Heap/numberContainerSystem.py
@@ -0,0 +1,28 @@
+class NumberContainers(object):
+
+    def __init__(self):
+        pass
+        
+
+    def change(self, index, number):
+        """
+        :type index: int
+        :type number: int
+        :rtype: None
+        """
+        pass
+        
+
+    def find(self, number):
+        """
+        :type number: int
+        :rtype: int
+        """
+        pass
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
