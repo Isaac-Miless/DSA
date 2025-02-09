4ef5cda2fcfb22e398eafa611864ac92d4a09c7c
Isaac <ibm4@kent.ac.uk>
2025-02-09 22:42:25 +0000

    Interesting solution

--- Heap/numberContainerSystem.py
+++ Heap/numberContainerSystem.py
@@ -1,25 +1,35 @@
 class NumberContainers(object):
 
     def __init__(self):
-        pass
+        self.mp = {} # number -> min heap of indices
+        self.map = {} # idx -> number
         
 
-    def change(self, index, number):
+    def change(self, index:int, number:int) -> None:
         """
         :type index: int
         :type number: int
         :rtype: None
         """
-        pass
+        self.map[index] = number
+        if number not in self.mp:
+            self.mp[number] = []
+        heapq.heappush(self.mp[number], index) # push index to min heap of indices
         
 
-    def find(self, number):
+    def find(self, number:int) -> int:
         """
         :type number: int
         :rtype: int
         """
-        pass
-        
+        if number not in self.mp: return -1
+        while self.mp[number]:
+            idx = self.mp[number][0]
+            if self.map[idx] == number:
+                return idx
+            heapq.heappop(self.mp[number]) # pop the index from min heap
+        return -1
+
 
 
 # Your NumberContainers object will be instantiated and called as such:
