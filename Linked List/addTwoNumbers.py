e8b350f005aecf730853d034a63cf6f2c0880ee4
Isaac <ibm4@kent.ac.uk>
2025-02-11 11:41:52 +0000

    Redo addTwoNumbers (I fully understand now)

--- Linked List/addTwoNumbers.py
+++ Linked List/addTwoNumbers.py
@@ -1,28 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-class Solution:
-    def addTwoNumbers(self, l1: 'Optional[ListNode]', l2: 'Optional[ListNode]') -> 'Optional[ListNode]':
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
         """
         :type l1: Optional[ListNode]
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        newLL = ListNode(0)
-        current = newLL
+        res = ListNode(0)
+        current = res
         carry = 0
         
         while l1 or l2 or carry:
             if l1:
                 carry += l1.val
-                l1 = l1.next
+                l1.next
             if l2:
                 carry += l2.val
-                l2 = l2.next
+                l2.next
             current.next = ListNode(carry % 10)
             current = current.next
-            carry = carry // 10
-        return newLL.next
+            carry //= 10
+        return res.next
