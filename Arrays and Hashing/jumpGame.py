2ccd24fdf355ee0474ebb6af17be924201c96dd5
Isaac <ibm4@kent.ac.uk>
2025-02-17 11:57:40 +0000

    jumpGame solution (not memory efficient)

--- Arrays and Hashing/jumpGame.py
+++ Arrays and Hashing/jumpGame.py
@@ -5,10 +5,23 @@ class Solution(object):
         :rtype: bool
         """
         # some dynamic programming solution here
-        def jump(i):
-            pass
+        n = len(nums)
+        memo = {}
         
-        pass
+        def compute(i):
+            if i >= n-1: return True
+            if nums[i] == 0: return False
+            if i in memo: return memo[i]
+            
+            for j in range(i+1, min(i + nums[i], n-1)+1):
+                if compute(j):
+                    memo[i] = True
+                    return True
+            memo[i] = False
+            return False
+        
+        return compute(0)
+        
 
 print(Solution().canJump([2,3,1,1,4])) # True
 print(Solution().canJump([3,2,1,0,4])) # False
