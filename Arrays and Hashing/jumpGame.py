a2efada08dba9782ec25a6c2c96836c0cbae1f63
Isaac <ibm4@kent.ac.uk>
2025-02-17 10:05:08 +0000

    Start Jump Game

---
+++ Arrays and Hashing/jumpGame.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # some dynamic programming solution here
+        def jump(i):
+            pass
+        
+        pass
+
+print(Solution().canJump([2,3,1,1,4])) # True
+print(Solution().canJump([3,2,1,0,4])) # False
