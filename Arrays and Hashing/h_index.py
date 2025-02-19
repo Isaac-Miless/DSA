73db422ef970daef213b6180062bd438fed53ebb
Isaac <ibm4@kent.ac.uk>
2025-02-19 11:53:11 +0000

    H-index finished

--- Arrays and Hashing/h_index.py
+++ Arrays and Hashing/h_index.py
@@ -6,11 +6,14 @@ class Solution(object):
         """
         n = len(citations)
         citations.sort()
-
+        
         # published at least x number of papers with x citations
         # h-index is the largest x
-        
-        pass
+
+        for i in range(n):
+            if citations[i] >= n-i:
+                return n-i
+        return 0
 
 citations = [3,0,6,1,5]
 sol = Solution()
