835b065cd78aca65ea51694f6ff71aef48cd41a5
Isaac <ibm4@kent.ac.uk>
2025-03-04 09:46:35 +0000

    Add nextGreatElem1

--- Stacks /nextGreaterElem1.py
+++ Stacks /nextGreaterElem1.py
@@ -5,4 +5,13 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        res = [-1] * len(nums1)
+        for num in nums1:
+            if num not in nums2: return -1
+            idx = nums2.index(num)
+            for i in range(idx, len(nums2)):
+                if num < nums2[i]:
+                    res[nums1.index(num)] = nums2[i]
+                    break
         
+        return res
