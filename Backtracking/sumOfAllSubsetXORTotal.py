e021104b0eb5d975a68719b1ede64efc95d3466a
Isaac <ibm4@kent.ac.uk>
2025-03-07 17:23:58 +0000

    Add backtracking question

---
+++ Backtracking/sumOfAllSubsetXORTotal.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def subsetXORSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def backtrack(self, subset, res):
+            pass
+
+        subsets = []
+        res = 0
+        
+        self.backtrack(subsets, res)
+        
+
+        return res
