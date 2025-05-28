04e78d3f126c3ed81164e5535affb9a6d520e922
Isaac <ibm4@kent.ac.uk>
2025-05-28 12:44:31 +0100

    Yesterday's Daily LC

---
+++ Arrays and Hashing/findWordsContainingChar.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+        # res = []
+        
+        # for i, word in enumerate(words):
+        #     if x in word:
+        #         res.append(i)
+                
+        # return res
+        
+        return [i for i, word in enumerate(words) if word.contains(x)]
